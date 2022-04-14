6ea6324f3d91af63a7a0c85b78ad0182a37597e9
Ann <es@kraski.ru>
2022-04-14 23:54:36 +0300

    добавили Task3

---
+++ Task3/Program.cs
@@ -0,0 +1,12 @@
+﻿// Прверяем чет-нечет
+Console.Write("Введите любое число: ");
+double a = Convert.ToDouble(Console.ReadLine());
+
+if (a % 2 == 0)
+{
+    Console.Write("Число чётное");
+}
+else
+{
+    Console.Write("Число нечётное");
+}
