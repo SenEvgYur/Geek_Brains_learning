96371bb496846c15fc0fd296f90d30f39e2da023
Ann <es@kraski.ru>
2022-04-15 14:08:33 +0300

    Порядок в папках

---
+++ Csharp/Homework1/Task1/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Write("Введите 1-е число: ");
+double a = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите 2-е число: ");
+double b = Convert.ToDouble(Console.ReadLine());
+
+double max = a;
+double min = b;
+
+if (b > a) 
+{
+    max = b; min = a;
+}
+
+Console.WriteLine("Результат сравнения:");
+Console.Write("max = ");
+Console.WriteLine(max);
+Console.Write("min = ");
+Console.WriteLine(min);
---
+++ Csharp/Homework1/Task3/Program.cs
@@ -0,0 +1,12 @@
+﻿// Проверяем чет-нечет
+Console.Write("Введите любое число: ");
+double a = Convert.ToDouble(Console.ReadLine());
+
+if (a % 2 == 0)
+{
+    Console.Write("Число чётное");
+}
+else
+{
+    Console.Write("Число нечётное");
+}
