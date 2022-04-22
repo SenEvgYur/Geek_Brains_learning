be8ef256b9c417e1661e8472943c36362e62add8
SenEvgYur <es@kraski.ru>
2022-04-22 17:10:18 +0300

    Добавили удаление второй цифры и проверяем выходной

---
+++ Csharp/Семинары/Homework2/Проверяем выходной/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая принимает на вход цифру, 
+// обозначающую день недели, и проверяет,является ли этот день выходным
+Console.Clear();
+Console.WriteLine("Введите цифру от 1 до 7: ");
+int x = Convert.ToInt32(Console.ReadLine());
+if (x > 0 & x < 6)
+{
+    Console.WriteLine("будний день");
+}
+if (x == 6 | x == 7)
+{
+    Console.WriteLine("выходной день");
+}
+if (x > 7 | x < 1)
+{
+    Console.WriteLine("Введите цифру заново");
+}
