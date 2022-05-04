daa89c980f11c69e1ad905a931a86a3c81ffeaef
SenEvgYur <es@kraski.ru>
2022-05-04 23:42:03 +0300

    Добавил Homework6 и решение задачи про кол-во цифр больше 0

---
+++ Csharp/Семинары/Homework6/Кол-во цифр больше нуля/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел.
+// Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// -1, -7, 567, 89, 223-> 3
+Console.Clear();
+Console.WriteLine("Введите любое количество цифр через пробел:");
+string M = Console.ReadLine();
+int count = 0;
+string[] mass = M.Split(" ");
+for (int i = 0; i < mass.Length; i++)
+{
+    int some  = int.Parse(mass[i]);
+    if (some > 0) count++;
+}
+System.Console.WriteLine(count);
