6562f4d48e46014df72729000c939e202ae92c24
SenEvgYur <es@kraski.ru>
2022-05-17 23:07:40 +0300

    начало задачи про массив с вещественными числами

---
+++ Csharp/Семинары/Homework7/Двумерный массив с вещественными числами/Program.cs
@@ -0,0 +1,36 @@
+﻿// Задача 47: Задайте двумерный массив размером m×n,
+// заполненный случайными вещественными числами.
+// m = 3, n = 4.
+// 0,5 7 -2 -0,2
+// 1 -3,3 8 -9,9
+// 8 7,8 -7,1 9
+void PrintArray(double[,] arr)  // функция печати двумерного массива
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            Console.Write(arr[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+void FillArray(double[,] arr) // функция заполнения двумерного массива
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            arr[i, j] = new Random().Next(1, 10);
+        }
+    }
+}
+Console.Clear();
+Console.Write("Введите количество строк в массиве: ");
+double m = double.Parse(Console.ReadLine());
+Console.Write("Введите количество столбцов в массиве: ");
+double n = double.Parse(Console.ReadLine());
+double[,] massive = new double[m, n]; // объявляем массив
+FillArray(massive);
+PrintArray(massive);
