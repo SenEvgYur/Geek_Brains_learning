604ca5d3f9c49e158859e0dfcb77634ce2590fbd
SenEvgYur <es@kraski.ru>
2022-05-18 23:38:18 +0300

    записано условие задачи про поиск числа по позиции

---
+++ Csharp/Семинары/Homework7/Вводим индекс и ищем число в двумерном массиве/Program.cs
@@ -0,0 +1,39 @@
+﻿// Задача 50: Напишите программу, которая на вход
+// принимает позиции элемента в двумерном массиве, и
+// возвращает значение этого элемента или же указание,
+// что такого элемента нет.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 17 -> такого числа в массиве нет
+void PrintArray(int[,] arr)  // функция печати двумерного массива
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            Console.Write(arr[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+void FillArray(int[,] arr) // функция заполнения двумерного массива
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            arr[i, j] = new Random().Next(1, 10);
+        }
+    }
+}
+Console.Clear();
+Console.Write("Введите количество строк в массиве: ");
+int m = int.Parse(Console.ReadLine());
+Console.Write("Введите количество столбцов в массиве: ");
+int n = int.Parse(Console.ReadLine());
+int[,] massive = new int[m, n]; // объявляем массив
+FillArray(massive);
+PrintArray(massive);
