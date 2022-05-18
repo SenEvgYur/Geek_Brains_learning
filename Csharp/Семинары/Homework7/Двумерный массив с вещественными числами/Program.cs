e515603d93a9582981e6bfcb58ec60500c250db3
SenEvgYur <es@kraski.ru>
2022-05-18 23:27:03 +0300

    решена задача про заполнение массива вещественными числами

--- Csharp/Семинары/Homework7/Двумерный массив с вещественными числами/Program.cs
+++ Csharp/Семинары/Homework7/Двумерный массив с вещественными числами/Program.cs
@@ -4,33 +4,34 @@
 // 0,5 7 -2 -0,2
 // 1 -3,3 8 -9,9
 // 8 7,8 -7,1 9
-void PrintArray(double[,] arr)  // функция печати двумерного массива
+void PrintArray(double[,] arr)  // функция печати двумерного массива c округлением до тысячных
 {
     for (int i = 0; i < arr.GetLength(0); i++)
     {
         for (int j = 0; j < arr.GetLength(1); j++)
         {
-            Console.Write(arr[i, j] + " ");
+            Console.Write(Math.Round(arr[i, j], 3) + "  ");
         }
         Console.WriteLine();
     }
     Console.WriteLine();
 }
-void FillArray(double[,] arr) // функция заполнения двумерного массива
+void FillArray(double[,] arr) // функция заполнения двумерного массива до 100
 {
     for (int i = 0; i < arr.GetLength(0); i++)
     {
         for (int j = 0; j < arr.GetLength(1); j++)
         {
-            arr[i, j] = new Random().Next(1, 10);
+            arr[i, j] = new Random().NextDouble()*100;
         }
     }
 }
 Console.Clear();
 Console.Write("Введите количество строк в массиве: ");
-double m = double.Parse(Console.ReadLine());
+int m = int.Parse(Console.ReadLine()!);
 Console.Write("Введите количество столбцов в массиве: ");
-double n = double.Parse(Console.ReadLine());
+int n = int.Parse(Console.ReadLine()!);
 double[,] massive = new double[m, n]; // объявляем массив
+Console.WriteLine("Массив: ");
 FillArray(massive);
 PrintArray(massive);
