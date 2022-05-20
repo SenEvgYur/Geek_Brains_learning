74bb4f8ac6d3e027994bd1992c17b8568b8ec15d
SenEvgYur <es@kraski.ru>
2022-05-20 18:01:06 +0300

    начали решать задачу про сортировку внутри строк

---
+++ Csharp/Семинары/Homework8/Сортировка внутри строки в двумерном массиве/Program.cs
@@ -0,0 +1,36 @@
+﻿// Задача 54: Задайте двумерный массив. Напишите программу, которая
+// упорядочит по убыванию элементы каждой строки двумерного массива.
+void PrintArray(int[,] arr)  // функция печати двумерного массива
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            Console.Write(arr[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+void FillArray(int[,] arr) // функция заполнения двумерного массива
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            arr[i, j] = new Random().Next(1, 10);
+        }
+    }
+}
+Console.Clear();
+Console.Write("Введите количество строк в массиве: ");
+int m = int.Parse(Console.ReadLine());
+Console.Write("Введите количество столбцов в массиве: ");
+int n = int.Parse(Console.ReadLine());
+int[,] massive = new int[m, n]; // объявляем массив
+FillArray(massive);
+PrintArray(massive);
+int temp; // вводим временную переменную для сортировки
+
+
+PrintArray(massive);
